6087a93835111ded7d7ac946e293989baab15cfb
dheeraj.sachan <dheeraj.sachan@aurusnet.com>
2014-09-28 21:41:58 +0530

    real time logging first step

--- src/main/java/np/dheeraj/sachan/transcoder/FfmpegRunnable.java
+++ src/main/java/np/dheeraj/sachan/transcoder/FfmpegRunnable.java
@@ -1,10 +1,12 @@
 package np.dheeraj.sachan.transcoder;
+
 import com.google.common.eventbus.EventBus;
 import np.dheeraj.sachan.Events.TrancodeCompleteEvent;
 import np.dheeraj.sachan.Events.TranscodeFailEvent;
 import np.dheeraj.sachan.Events.TranscodingFileEvent;
 import np.dheeraj.sachan.videoTranscoder.ExecLogHandler;
 import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.log4j.Level;
@@ -25,49 +27,68 @@ public class FfmpegRunnable implements Runnable {
     private ArrayBlockingQueue<ConversionTask> conversionTasks;
     private EventBus eventBus;
     private static final Logger logger = LoggerFactory.getLogger(FfmpegRunnable.class);
-    public FfmpegRunnable(ArrayBlockingQueue<ConversionTask> tasks,EventBus eventBus) {
+
+    public FfmpegRunnable(ArrayBlockingQueue<ConversionTask> tasks, EventBus eventBus) {
         this.conversionTasks = tasks;
         this.eventBus = eventBus;
         this.eventBus.register(this);
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
 
-        if(conversionTasks.isEmpty())
-        {
+        if (conversionTasks.isEmpty()) {
             throw new RuntimeException("tasks finished");
         }
         String outputFile = "";
         String inputFile = "";
         ConversionTask conversionTask = null;
-        try{
+        try {
             conversionTask = conversionTasks.take();
             String command = conversionTask.getCommandToExecute();
             outputFile = conversionTask.getOutPutFile();
             inputFile = conversionTask.getFileName();
             eventBus.post(new TranscodingFileEvent(inputFile));
-            CommandLine cmdLine = CommandLine.parse(command);
+            final CommandLine cmdLine = CommandLine.parse(command);
             logger.info("Executing FFMPEG command " + command);
-            DefaultExecutor executor = new DefaultExecutor();
-                PumpStreamHandler psh = new PumpStreamHandler(new ExecLogHandler(logger, Level.INFO), new ExecLogHandler(logger, Level.ERROR));
-                executor.setStreamHandler(psh);
-                executor.execute(cmdLine);
-        }
-        catch (Exception e)
-        {
-            logger.error("caught exception "+e);
-        }  finally {
-        File file = new File(outputFile.replace("\"",""));
-        if (file.exists() && file.length() > 200) {
-            logger.error("FFMPEG CONVERSION SUCCESS FOR FILE " + outputFile);
-            eventBus.post(new TrancodeCompleteEvent(inputFile));
-        } else {
-            logger.error("FFMPEG CONVERSION FAILED FOR FILE " + outputFile);
-            //transocde failed
-            eventBus.post(new TranscodeFailEvent(inputFile));
+            final DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
+            final DefaultExecutor executor = new DefaultExecutor();
+            PumpStreamHandler psh = new PumpStreamHandler(new ExecLogHandler(logger, Level.INFO)/*, new ExecLogHandler(logger, Level.ERROR)*/);
+            executor.setStreamHandler(psh);
+            Executors.newSingleThreadExecutor().execute(new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                    executor.execute(cmdLine, resultHandler);
+                    }catch (Exception e){
+                        logger.error("INSIDE EXCEPTION " + e.getMessage());
+                    }
+                }
+            });
+            logger.info("(((((((((((((((((((((((((((((((((((((((((((((((((((((((((");
+        } catch (Exception e) {
+            logger.error("caught exception " + e);
+        } finally {
+            File file = new File(outputFile.replace("\"", ""));
+            if (file.exists() && file.length() > 200) {
+                logger.error("FFMPEG CONVERSION SUCCESS FOR FILE " + outputFile);
+                eventBus.post(new TrancodeCompleteEvent(inputFile));
+            } else {
+                logger.error("FFMPEG CONVERSION FAILED FOR FILE " + outputFile);
+                //transocde failed
+                eventBus.post(new TranscodeFailEvent(inputFile));
+            }
         }
     }
+
+    private class TranscoderStatusUpdator implements Runnable {
+
+        private File outputFile;
+
+
+        @Override
+        public void run() {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
     }
 }
--- src/main/java/np/dheeraj/sachan/videoTranscoder/ExecLogHandler.java
+++ src/main/java/np/dheeraj/sachan/videoTranscoder/ExecLogHandler.java
@@ -4,12 +4,14 @@ import org.apache.commons.exec.LogOutputStream;
 import org.apache.log4j.Level;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+
 /**
- * Created by sujeet on 12/16/13.
+ * Created by dheeraj on 12/16/13.
  */
 public class ExecLogHandler extends LogOutputStream {
     private Logger logger;
-
+    private boolean skip;
     public ExecLogHandler(Logger logger, Level level) {
         super(level.toInt());
         this.logger = logger;
@@ -17,7 +19,7 @@ public class ExecLogHandler extends LogOutputStream {
 
     @Override
     protected void processLine(String line, int level) {
-        switch (level) {
+       /* switch (level) {
             case Level.ERROR_INT:
                 logger.info(line);
                 break;
@@ -25,8 +27,13 @@ public class ExecLogHandler extends LogOutputStream {
             default:
                 logger.error(line);
                 break;
-        }
+        }*/
+        System.out.println(line);
+    }
 
-
+    @Override
+    public void write(final int cc) throws IOException {
+        final byte c = (byte) cc;
+        System.out.println("***********"+cc+"");
     }
 }
